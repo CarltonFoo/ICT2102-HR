b7cb834e30cda093aa6fabda36e1e6623396288e
bellesim <bellesimbeier@gmail.com>
2021-11-04 08:45:29 +0800

    altered top navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import useStyles from "./navbarStyle";
 import { Link } from "react-router-dom";
-import Modal from 'react-modal';
-
-
+import Modal from "react-modal";
 import { Menu } from "antd";
 import {
   AppstoreOutlined,
@@ -14,6 +12,7 @@ import {
 import "../../assets/css/navbar.css";
 import employees from "../../data/employees.json";
 import { MenuItem } from "rc-menu";
+import { BellFilled } from "@ant-design/icons";
 
 const { SubMenu } = Menu;
 
@@ -36,24 +35,9 @@ const Navbar = () => {
       <div class=" max-screen h-16 mx-auto  bg-blue-800">
         <div class="flex pt-4 justify-between">
           <h2 class="text-2xl text-white font-bold pl-12 ">WorkDay</h2>
-          <Menu
-            mode="horizontal"
-            openKeys={openKeys}
-            onOpenChange={onOpenChange}
-          >
-            <MenuItem key="sub1" icon={<MailOutlined />}>
-              Dashboard
-            </MenuItem>
-            <MenuItem key="sub2" icon={<MailOutlined />}>
-              Schedule
-            </MenuItem>
-            <MenuItem key="sub3" icon={<MailOutlined />}>
-              Leaves
-            </MenuItem>
-            <MenuItem key="sub4" icon={<MailOutlined />}>
-              Payslip
-            </MenuItem>
-          </Menu>
+          <div class="mr-10 pt-2">
+            <BellFilled style={{ fontSize: "1.5rem", color: "#ffffff" }} />
+          </div>
         </div>
       </div>
       <div class=" w-64 bg-white   h-screen">
