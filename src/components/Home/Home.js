c5723f4a7a60e7f882463f51cc972ad51d77d131
170joeychua <170joeychua@gmail.com>
2021-11-10 02:29:25 +0800

    Add top 5 staff availability and refine pie chart and refine card content
    pie chart and availabilty can retrieve from JSON. Cardview for countdown still incomplete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,62 +1,127 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import { Link } from "react-router-dom";
 import ReactTooltip from 'react-tooltip';
-import Modal from 'react-modal';
-import { Button, Card, Col, Row } from "antd";
-import Navbar from "../Navbar/Navbar";
+import { Table, Card, Col, Row } from "antd";
+import Mood from "../Mood/Mood"
 import { Pie } from '@ant-design/charts';
-import { InfoCircleOutlined, EyeFilled } from '@ant-design/icons';
-
+import { InfoCircleTwoTone, EyeFilled } from '@ant-design/icons';
+import CountDownTimer from "../CoundownTimer/CountDownTimer";
 import "../../assets/css/home.css"
 
-var data = [
+import StaffAvailability from "../../data/staffAvailability.json";
+import PayslipJSON from "../../data/payslip.json";
+
+
+
+// START Availability
+const columns = [
   {
-    type: 'Slice 1',
-    value: 27,
+    title: "Name",
+    dataIndex: "name",
   },
   {
-    type: 'Slice 2',
-    value: 25,
+    title: "Position",
+    dataIndex: "position",
   },
   {
-    type: 'Slice 3',
-    value: 18,
+    title: "Leave Start Date",
+    dataIndex: "leaveStartDate",
   },
   {
-    type: 'Slice 4',
-    value: 15,
+    title: "Leave End Date",
+    dataIndex: "leaveEndDate",
   },
   {
-    type: 'Slice 5',
-    value: 10,
+    title: "Leave Type",
+    dataIndex: "leaveType",
   },
   {
-    type: 'Slice 6',
-    value: 5,
+    title: "Covering Person",
+    dataIndex: "coveringPerson",
+  },
+];
+
+// END Availability
+
+// START Payslip
+var totalClaims = 0;
+var totalEarnings = PayslipJSON[0].earnings.basicPay + PayslipJSON[0].earnings.bonus + PayslipJSON[0].earnings.OTpay;
+var totalDeductions = PayslipJSON[0].deductions.CPFcontribution + PayslipJSON[0].deductions.taxDeduction;
+
+Object.keys(PayslipJSON[0].claims).map((claim) =>
+  totalClaims += PayslipJSON[0].claims[claim].claimAmt
+)
+
+var totalOverall = totalEarnings + totalClaims - totalDeductions;
+
+var piedata = [
+  {
+    type: 'Base Pay',
+    value: PayslipJSON[0].earnings.basicPay,
+  },
+  {
+    type: 'Bonus Pay',
+    value: PayslipJSON[0].earnings.bonus,
+  },
+  {
+    type: 'OT Pay',
+    value: PayslipJSON[0].earnings.OTpay,
+  },
+  {
+    type: 'Claims',
+    value: totalClaims,
+  },
+  {
+    type: 'CPF',
+    value: PayslipJSON[0].deductions.CPFcontribution,
+  },
+  {
+    type: 'Tax',
+    value: PayslipJSON[0].deductions.taxDeduction,
   },
 ];
 
 var config = {
-  appendPadding: 10,
-  data: data,
+  data: piedata,
   angleField: 'value',
   colorField: 'type',
-  radius: 0.9,
+  radius: 0.5,
+  height: 340,
+  legend: {
+    layout: 'vertical',
+    position: 'bottom',
+    flipPage: false,
+    itemName: {
+      style: {
+        fontSize: 16,
+      }
+    }
+  },
   label: {
-    type: 'inner',
-    offset: '-30%',
-    content: function content(_ref) {
-      var percent = _ref.percent;
-      return ''.concat((percent * 100).toFixed(0), '%');
-    },
+    type: 'spider',
+    labelHeight: 40,
+    content: '{name}\n${value}',
     style: {
-      fontSize: 14,
+      fontSize: 12,
       textAlign: 'center',
     },
   },
-  interactions: [{ type: 'element-active' }],
+  interactions: [{ type: 'element-selected' }, { type: 'element-active' }],
 };
+
+var linkStyle = {
+  position: 'absolute',
+  bottom: 0,
+  right: 10,
+  marginBottom: 10
+}
+
+var cardStyle = {
+  fontSize: 20,
+  textAlign: 'center',
+  fontWeight: 'bold'
+}
+// END Payslip
 
 const gridStyle = {
   width: '25%',
@@ -72,48 +137,67 @@ const Home = () => {
         <div class="text-2xl font-bold my-6">Dashboard
           {/* Need to add hover styling and tooltip*/}
           <div data-tip="Quick overview of team availability, salary" class="inline">
-            <InfoCircleOutlined class="inline-block" className={"px-4"} />
+            <InfoCircleTwoTone twoToneColor="#A3A989" class="inline-block" className={"px-4"} />
           </div>
-          <ReactTooltip  place="right" effect="solid" />
-          
+          <ReactTooltip place="right" effect="solid" />
+
+        </div>
+        <div className="site-card-wrapper">
+          {PayslipJSON && PayslipJSON.length > 0 && PayslipJSON.map((data) =>
+            <Row gutter={16}>
+              <Col span={8}>
+                <Card style={cardStyle} bordered={true}>
+                  6<br></br>
+                  Welfare Pack Requests</Card>
+              </Col>
+              <Col span={8}>
+                <Card style={cardStyle} bordered={true}>
+                  {data.user.remainingAnnualLeave}<br></br>
+                  Annual Leave Left</Card>
+              </Col>
+              <Col span={8}>
+                <Card style={cardStyle} bordered={true}>
+                  <div class="countdown">
+                    <CountDownTimer></CountDownTimer>
+                  </div>
+                  Days to Pay Day</Card>
+              </Col>
+            </Row>
+          )}
         </div>
         <div className="site-card-wrapper">
           <Row gutter={16}>
-            <Col span={8}>
-              <Card bordered={true}>6<br></br>Welfare Pack Requests</Card>
+            <Col span={16}>
+              <Card
+                style={{ marginTop: 16, height: 520 }}
+                type="inner"
+                title="Staff Availability"
+              >
+                <Table pagination={false} columns={columns} dataSource={StaffAvailability.slice(5, 10)} />
+                <Link to="/availability"
+                  style={ linkStyle }>
+                  View All Staff Availability &#62;</Link>
+              </Card>
             </Col>
+
             <Col span={8}>
-              <Card bordered={true}>10<br></br>Annual Leave Left</Card>
-            </Col>
-            <Col span={8}>
-              <Card bordered={true}>5<br></br>Days to Pay Day</Card>
+              <Card
+                style={{ marginTop: 16, height: 520 }}
+                type="inner"
+                title="Salary Breakdown"
+                extra={<EyeFilled data-tip="Click to hide total salary" style = {{ cursor: 'pointer' }}/> }
+              >
+                <Pie {...config} />
+                <div class="p-6 font-bold text-center">
+                  Total: ${totalOverall.toFixed(2)}
+                </div>
+                <Link to="/payslip"
+                  style={ linkStyle }>
+                  View Full Summary &#62;</Link>
+              </Card>
             </Col>
           </Row>
-        </div>
-        <div className="site-card-wrapper">
-          <Row gutter={16}>
-        <Col span={12}>
-          <Card
-            style={{ marginTop: 16 }}
-            type="inner"
-            title="Staff Availability"
-          >
-            <Link to="/availability">View All Staff Availability &#62;</Link>
-          </Card>
-        </Col>
-
-        <Col span={12}>
-          <Card
-            style={{ marginTop: 16 }}
-            type="inner"
-            title="Salary Breakdown"
-            extra={<EyeFilled />}
-          >
-            <Pie {...config} />
-            <Link to="/payslip">View Full Summary &#62;</Link>
-          </Card>
-        </Col>
-        </Row>
+          <Mood></Mood>
         </div>
       </div>
     </div>
--- src/components/Mood/Mood.js
+++ src/components/Mood/Mood.js
@@ -1,14 +1,8 @@
 import React from "react";
-import useStyles from "./moodStyle";
-import { Link } from "react-router-dom";
-import ReactTooltip from 'react-tooltip';
-import Modal from 'react-modal';
 import { Rate } from 'antd';
 import { FrownOutlined, MehOutlined, SmileOutlined } from '@ant-design/icons';
 
-
 const Mood = () => {
-
   const customIcons = {
     1: <FrownOutlined />,
     2: <FrownOutlined />,
@@ -16,17 +10,11 @@ const Mood = () => {
     4: <SmileOutlined />,
     5: <SmileOutlined />,
   };
-  
 
   return (
     <div>
-    <div>
-        Mood
+      <Rate defaultValue={5} character={({ index }) => customIcons[index + 1]} />
     </div>
-        <div>
-        <Rate defaultValue={5} character={({ index }) => customIcons[index + 1]} />
-      </div>
-      </div>
   );
 };
 
