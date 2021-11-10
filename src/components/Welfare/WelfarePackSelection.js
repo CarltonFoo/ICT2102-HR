0b7fef12b7574b70d1f422bebe1dfa2cdfd52534
bellesim <bellesimbeier@gmail.com>
2021-11-11 00:15:39 +0800

    retreiving welfare name

--- src/components/Welfare/WelfarePackSelection.js
+++ src/components/Welfare/WelfarePackSelection.js
@@ -4,25 +4,31 @@ import { Link } from "react-router-dom";
 import ReactTooltip from "react-tooltip";
 import Modal from "react-modal";
 import WelfarePack from "../../data/welfare.json";
-import { Card, Col, Row, Image, Button } from "antd";
+import { Card, Col, Row, Image, Button, Form } from "antd";
 import { StepPanel } from "./StepPanel";
 
 const WelfarePackSelection = () => {
   //   const { setStep, welfareData, setwelfareData } = useContext(multiStepContext);
+  const retrieveData = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    console.log(e.target.title);
+  };
 
   return (
     <div>
       <Row gutter={16}>
         {WelfarePack.map((data) => {
+          console.log(data);
           return (
             <Col span={8} xs={24} xl={8}>
               <Card
                 title={data.welfarePack}
+                value={data.welfarePack}
                 hoverable={true}
                 style={{ textAlign: "center", margin: "5%" }}
-              >
-                Card content
-              </Card>
+                onClick={retrieveData}
+              ></Card>
             </Col>
           );
         })}
