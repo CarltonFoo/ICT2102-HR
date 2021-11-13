ab5fc963f9db391655681adb17c225cb8a684cdb
170joeychua <170joeychua@gmail.com>
2021-11-13 16:48:32 +0800

    update mood add selected status message and twotonemood

--- src/components/Mood/Mood.js
+++ src/components/Mood/Mood.js
@@ -1,19 +1,35 @@
 import React from "react";
 import Tween, { TweenOneGroup } from 'rc-tween-one';
-import { Button, Tooltip } from 'antd';
-import { FrownOutlined, MehOutlined, SmileOutlined, CloseOutlined } from '@ant-design/icons';
+import { message, Button, Tooltip } from 'antd';
+import { FrownOutlined, FrownTwoTone, MehOutlined, MehTwoTone, SmileOutlined, SmileTwoTone, CloseOutlined } from '@ant-design/icons';
+
+
 
 const customIcons = [
-  <FrownOutlined style={{ fontSize: 28 }} />,
-  <MehOutlined style={{ fontSize: 28 }}  />,
-  <SmileOutlined style={{ fontSize: 28 }}  />,]
+  <FrownOutlined style={{ fontSize: 40 }} />,
+  <MehOutlined style={{ fontSize: 40 }} />,
+  <SmileOutlined style={{ fontSize: 40 }} />,
+]
+
+const customIconsTwoTone = [
+  <FrownTwoTone style={{ fontSize: 40 }} twoToneColor="#3b82f6" />,
+  <MehTwoTone style={{ fontSize: 40 }} twoToneColor="#3b82f6" />,
+  <SmileTwoTone style={{ fontSize: 40 }} twoToneColor="#3b82f6" />
+]
+
+const customMessage = [
+  "moody~",
+  "so-so.",
+  "happy!"
+]
 
 class Mood extends React.Component {
   state = {
     isActive: false,
     smileyFace: customIcons[2],
-
   };
+
+
 
   handleShow = () => {
     this.setState({ isActive: true });
@@ -24,43 +40,54 @@ class Mood extends React.Component {
   };
 
   handleSelect = (i) => {
-    console.log(i);
-    this.setState({ isActive: false, smileyFace: customIcons[i] });
+    this.setState({ isActive: false, smileyFace: customIconsTwoTone[i] });
+    message.open({
+      content: 'Feeling ' + customMessage[i],
+      duration: 3,
+      icon: customIconsTwoTone[i],
+      style: {
+        marginTop: '20vh',
+      },
+    });
   };
 
-  handleRatingChange(value) {
-    console.log(value);
-  }
   render() {
     return (
       <div>
         <div >
           <div data-tip="Close" class="inline">
-            <Button type="primary" shape="circle" size="large" style={{ cursor: 'pointer', position: 'absolute', bottom: 20, right: 40, zIndex: 1 }}
-              onClick={this.handleHide} icon={<CloseOutlined></CloseOutlined>} />
+            {/* // ask what tailwind color */}
+            <button class="bg-yellow-300 hover:bg-yellow-500 text-black text-center rounded-full h-14 w-14 items-center shadow-xl"
+              style={{ cursor: 'pointer', position: 'absolute', bottom: 20, right: 42, zIndex: 1 }}
+              onClick={this.handleHide}><CloseOutlined style={{ fontSize: 28 }}></CloseOutlined></button>
           </div>
         </div>
-        <TweenOneGroup >
-          {this.state.isActive ? (
-              <Tween
-                animation={{ x: -50 }}
-                style={{ opacity: 1, position: 'absolute', bottom: 100, right: 40, zIndex: 1 }}
-              >
-                {customIcons.map((value, index) => {
-                  return <button onClick={() => this.handleSelect(index)} 
-                  style={{ background: '#f4f4f4', border: '2px solid black', borderRadius: 50, padding: 5 }} 
-                  onMouseEnter={({ target }) => target.style.color = '#F9C80E'} 
-                  onMouseLeave={({ target }) => target.style.color = '#000000'} key={index}>{value}</button>
-                })}
-              </Tween>
-          ) : (
-            <div data-tip="How are you feeling today?" class="inline">
-              <Button type="primary" shape="circle" size="large" style={{ cursor: 'select', position: 'absolute', bottom: 20, right: 40, zIndex: 1, color: '#000000' }}
-                onClick={this.handleShow} icon={this.state.smileyFace} />
-            </div>
-          )}
-        </TweenOneGroup>
-      </div>);
+        {this.state.isActive ? (
+                      <Tween
+                      animation={{ x: -50 }}
+                      style={{ opacity: 1, zIndex: 1, position: 'absolute', bottom: 20, right: 50 }}
+                    >
+          <div style={{ border: '2px solid black', borderRadius: 50, width: 250, height: 60, zIndex: 1, textAlign: 'center', backgroundColor: 'white'}}>
+              {customIcons.map((value, index) => {
+                return <button class="bg-white-300 hover:bg-white-500 text-black text-center rounded-full h-14 w-14 items-center shadow-xl"
+                  onClick={() => this.handleSelect(index)}
+                  style={{ marginLeft: 10,  padding: 5 }}
+                  onMouseEnter={({ target }) => target.style.color = '#F9C80E'}
+                  onMouseLeave={({ target }) => target.style.color = '#000000'}
+                  key={index}>{value}</button>
+              })}
+          </div>
+          </Tween>
+        ) : (
+          <div data-tip="How are you feeling today?" class="inline">
+            <button class="bg-yellow-300 hover:bg-yellow-500 text-black text-center rounded-full h-14 w-14 items-center shadow"
+              style={{ cursor: 'select', position: 'absolute', bottom: 20, right: 42, zIndex: 1, color: '#000000' }}
+              onClick={this.handleShow} >
+              {this.state.smileyFace}
+            </button>
+          </div>
+        )}
+      </div >);
   }
 }
 
