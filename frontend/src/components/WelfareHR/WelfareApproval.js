1524b33ff2c7e2abce425d4e5c71eb4d2ddda872
Jun Kiat Ng <ngjunkiat968@gmail.com>
2021-11-14 21:38:03 +0800

    updated WelfareApproval ui

--- frontend/src/components/WelfareHR/WelfareApproval.js
+++ frontend/src/components/WelfareHR/WelfareApproval.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { Link } from "react-router-dom";
 import ReactTooltip from 'react-tooltip';
 import Modal from 'react-modal';
-import { Popover, Popconfirm, Tag, Table, Button, Card, Col, Row } from "antd";
+import { Popover,Popconfirm, Tag, Table, Button, Card, Col, Row } from "antd";
 import approvalData from "../../data/approval.json";
 import { getComponentController } from "@antv/g2/lib/chart/controller";
 import "./approvalstyle.css";
 import { fixedBase } from "@antv/util";
 import { removeWelfareRequest, updateStock } from "../../api";
-// import { updateWelfareInventory } from "../../api";
+
 
 class WelfareApproval extends React.Component {
   tablerecords = [];
@@ -17,9 +17,17 @@ class WelfareApproval extends React.Component {
     visible: false,
     selectedRowKeys: [], // Check here to configure the default column
   };
+  hide = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+  handleVisibleChange = visible => {
+    this.setState({ visible });
+  };
 
-  onSelectChange = (selectedRowKeys) => {
-    console.log("selectedRowKeys changed: ", selectedRowKeys);
+  onSelectChange = selectedRowKeys => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
     this.setState({ selectedRowKeys });
   };
 
@@ -40,70 +48,75 @@ class WelfareApproval extends React.Component {
       },
     ];
     this.state = {
-      dataSource: approvalData,
+      dataSource: approvalData
     };
   }
-  getSelectedPackages = async () => {
+  updateJSONdata = async () => {
     //get all the data
     // const dataSource = [...this.state.dataSource];
-    //creation of empty array
     var temp = [];
-
     //pass selected key
     if (typeof this.state.selectedRowKeys !== "undefined") {
-      // temp = dataSource.filter((item) =>
-      //   this.state.selectedRowKeys.includes(item.key)
-      // );
-
       const selectedKey = [...this.state.selectedRowKeys];
-
       console.log("selectedPackagesArr: ", selectedKey);
-      // var giftText = "";
-      // for (var i = 0; i < temp.length; i++) {
-      //   giftText =
-      //     giftText + "Gift type no: " + i + " , " + temp[i].gifttype + "\n";
-      // }
-      // console.log("GIFT TEXT:\n", giftText);
-      // if (giftText == "") return "Please select items to approve.";
-      // return giftText;
       var temp = this.state.dataSource.filter((item) => selectedKey.includes(item.key));
-      // console.log("temp data getting approved rows",tempdata);
       // storing gift type+ count 
       var _ = require('underscore')
       var countArr = _.countBy(temp, function (temp) { return temp.gifttype });
       countArr.selectedKeys = selectedKey;
       console.log("countArr",countArr);
       removeWelfareRequest(countArr);
-      // const res1 = await removeWelfareRequest(countArr);
-      // console.log("res1", res1);
-      // if (res1.status === 200) {
-      //   console.log("status 200");
-      // }
-      // Tell the browser that this function is asynchronous
-      // async function myFunc() {
-      //   // Await for the promise to resolve
-      //   await new Promise((resolve) => {
-      //     setTimeout(() => {
-      //       // Resolve the promise
-      //       resolve(removeWelfareRequest(selectedKey));
-      //     }, 3000);
-      //   });
-      //   // Once the promise gets resolved continue on
-      // }
-      // // Call the function
-      // myFunc();
-      // updateStock(countArr);
-
-      // const res2 = await updateStock(countArr);
-      // console.log("res2", res2);
-      // if (res2.status === 200) {
-      //   console.log("status 200");
-      // }
     } else {
       // if no items selected
       return "Please select items to approve.";
     }
   };
+  getSelectedPackages = (key) =>{
+    const dataSource = [...this.state.dataSource];
+    var temp = [];
+    
+    //if items are selected 
+    if (typeof this.state.selectedRowKeys !== 'undefined'){
+      temp = dataSource.filter(item => this.state.selectedRowKeys.includes(item.key));
+      // console.log("selectedPackagesArr: ",temp);
+      var giftText = ""
+
+      // for(var i = 0; i < temp.length ; i++){
+      //   giftText = giftText + "Gift type no: "+ i + " , "+ temp[i].gifttype + '\n';
+      // }
+      var _ = require('underscore')
+      var countArr = _.countBy(temp, function(temp) { return temp.gifttype });
+
+      for(var giftname in countArr){
+        giftText = giftText + countArr[giftname] + " x " + giftname + "\n";
+      }
+
+      if (giftText == "") return "Please select items to approve."
+      return giftText;
+    }
+    else{
+      // if no items selected
+    return "Please select items to approve."
+    }
+  }
+  handleDelete = () => {
+    console.log("handle delete");
+    const dataSource = [...this.state.dataSource];
+    console.log("datasource:", dataSource);
+    console.log("selectedRows", this.state.selectedRowKeys);
+    //error check ensure selectedkeys initialized
+    if (typeof this.state.selectedRowKeys !== 'undefined'){
+    this.setState({
+      dataSource: dataSource.filter(item =>
+        !this.state.selectedRowKeys.includes(item.key)
+      ),
+    });
+    //update backend json file
+    this.updateJSONdata();
+    }
+    this.hide()
+  };
+
 
   render() {
     const { selectedRowKeys } = this.state;
@@ -113,27 +126,28 @@ class WelfareApproval extends React.Component {
         return col;
       }
 
-      // return {
-      //   ...col,
-      //   onCell: (record) => ({
-      //     record,
-      //     editable: col.editable,
-      //     dataIndex: col.dataIndex,
-      //     title: col.title,
-      //     handleSave: this.handleSave,
-      //   }),
-      // };
+      return {
+        ...col,
+        onCell: (record) => ({
+          record,
+          editable: col.editable,
+          dataIndex: col.dataIndex,
+          title: col.title,
+          handleSave: this.handleSave,
+        }),
+      };
     });
     const rowSelection = {
       selectedRowKeys,
       onChange: this.onSelectChange,
       selections: [
         Table.SELECTION_ALL,
+        // Table.SELECTION_INVERT,
         Table.SELECTION_NONE,
         {
-          key: "odd",
-          text: "Select Odd Row",
-          onSelect: (changableRowKeys) => {
+          key: 'odd',
+          text: 'Select Odd Row',
+          onSelect: changableRowKeys => {
             let newSelectedRowKeys = [];
             newSelectedRowKeys = changableRowKeys.filter((key, index) => {
               console.log(index);
@@ -146,9 +160,9 @@ class WelfareApproval extends React.Component {
           },
         },
         {
-          key: "even",
-          text: "Select Even Row",
-          onSelect: (changableRowKeys) => {
+          key: 'even',
+          text: 'Select Even Row',
+          onSelect: changableRowKeys => {
             let newSelectedRowKeys = [];
             newSelectedRowKeys = changableRowKeys.filter((key, index) => {
               console.log(index);
@@ -163,26 +177,41 @@ class WelfareApproval extends React.Component {
       ],
     };
 
+    
     return (
       <div class="m-auto w-11/12">
         <p class="text-2xl font-bold my-6">Welfare Approval</p>
-        <Table
-          rowSelection={rowSelection}
-          columns={columns}
-          dataSource={dataSource}
-        />
-
+        <Table rowSelection={rowSelection} columns={columns} dataSource={dataSource} />
+        <Popover
+        overlayInnerStyle={{textAlign:"center",whiteSpace: "pre-line",width:'60vw',height:'20vw'}}
+        content={
+        <>
+        <p class="font-bold text-center text-blue-800">Selected Gifts to approve</p>
+        <p>{this.getSelectedPackages()}</p>
+        <Button type="primary" onClick={()=>this.hide()} style={{position:"absolute",bottom:"3vw",left:"15vw"}}>Back</Button>
+        <Button type="primary"onClick={()=>this.handleDelete()}  style={{position:"absolute",bottom:"3vw",right:"15vw"}}>Approve</Button>
+        {/* <a onClick={this.handleDelete}>Approve</a> */}
+        </>
+        }
+        title={<b>Confirm Approval</b>}
+        trigger="click"
+        visible={this.state.visible}
+        onVisibleChange={this.handleVisibleChange}
+       >
         <Button
           style={{ margin: "0 30vw" }}
           type="primary"
-          onClick={() => this.getSelectedPackages()}
+          onClick={()=>this.getSelectedPackages()}
         >
           Approve
         </Button>
+        </Popover>
         {/* </Popconfirm> */}
       </div>
+
     );
   }
+
 }
 
 
